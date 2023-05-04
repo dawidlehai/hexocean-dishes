714f62ca1ef1d2f7c7456d332375ef45436b693b
dawidlehai <haidawidle@gmail.com>
2023-05-04 18:39:36 +0200

    fix: uncomment commented code

--- src/App.tsx
+++ src/App.tsx
@@ -16,26 +16,22 @@ function App() {
   const sendDataHandler: FormSubmitHandler<object> = async (
     formData: object
   ) => {
-    // const data = formData as DishFormValues;
+    const data = formData as DishFormValues;
 
-    // const dataFiltered: DishFormValues = {
-    //   name: data.name,
-    //   preparation_time: data.preparation_time,
-    //   type: data.type,
-    // };
-
-    // if (data.type === "pizza") {
-    //   dataFiltered.no_of_slices = data.no_of_slices;
-    //   dataFiltered.diameter = data.diameter;
-    // }
-    // if (data.type === "soup")
-    //   dataFiltered.spiciness_scale = data.spiciness_scale;
-    // if (data.type === "sandwich")
-    //   dataFiltered.slices_of_bread = data.slices_of_bread;
-
-    const data = {
-      name: "wadad",
+    const dataFiltered: DishFormValues = {
+      name: data.name,
+      preparation_time: data.preparation_time,
+      type: data.type,
     };
+
+    if (data.type === "pizza") {
+      dataFiltered.no_of_slices = data.no_of_slices;
+      dataFiltered.diameter = data.diameter;
+    }
+    if (data.type === "soup")
+      dataFiltered.spiciness_scale = data.spiciness_scale;
+    if (data.type === "sandwich")
+      dataFiltered.slices_of_bread = data.slices_of_bread;
 
     try {
       const options = {
